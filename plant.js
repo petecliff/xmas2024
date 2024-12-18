9581a1c1f43a1dc4140165b4170b1a11d0f203a7
Peter Cliff <pete@petecliff.net>
2024-12-18 21:41:29 +0000

    first commit

---
+++ plant.js
@@ -0,0 +1,80 @@
+class Plant {
+    word;
+    len;
+    angle;
+
+    constructor(xpos, ypos) {
+        this.word = "S";
+        this.len = 4.5;
+        this.angle = 0.1 * PI;
+        this.xpos = width*xpos;
+        this.ypos = height-ypos;
+        this.gen = 0;
+        this.sWeight = 20;
+    }
+
+    display() {
+        for(let i = 0; i < this.word.length; i++) {
+            switch(this.word.charAt(i)) {
+                case 'F':
+                    strokeWeight(this.sWeight);
+                    line(0,0,this.len,0);
+                    translate(this.len,0);
+                    break;
+                case 'l':
+                    rotate(-this.angle);
+                    break;
+                case 'r':
+                    rotate(this.angle);
+                    break;
+                case 'u':
+                    this.sWeight -= 3;
+                    break;            
+                case 'd':
+                    this.sWeight += 3;
+                    break;        
+                case '[':
+                    push();
+                    break;
+                case ']': 
+                    pop();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    expand() {
+        // if (this.gen > 10) {
+        //     return;
+        // }
+        //this.gen++;
+        let newStr = "";
+        for(let i = 0; i < this.word.length; i++) {
+            switch(this.word.charAt(i)) {
+                case 'S':
+                    newStr += "FB";
+                    break;
+                case 'F':
+                    if (random(100) < 55) {
+                        newStr += "FF";
+                    } else {
+                        newStr += "F";
+                    }
+                    break;
+                case 'B':
+                    if (random(100) < 50) {
+                        newStr += "[ullFBd][urFBd]";
+                    } else {
+                        newStr += "[ulFBd][urrFBd]";
+                    }
+                    break;
+                default:
+                    newStr += this.word.charAt(i);
+                    break;
+            }
+        }
+        this.word = newStr;
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,122 @@
+let c;
+let wood;
+let snow = [];
+let gravity;
+let happy;
+let christmas;
+let points;
+
+let spritesheet;
+let textures = [];
+
+let zOff = 0;
+
+let font;
+
+function preload() {
+    spritesheet = loadImage('data/flakes32.png');
+    font = loadFont("data/berkshireswash.ttf");
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    //happy = getFontOutlinePointsForWord("Happy");
+	//christmas = getFontOutlinePointsForWord("Christmas");
+
+    gravity = createVector(0, 0.3);
+
+    for (let x = 0; x < spritesheet.width; x += 32) {
+        for (let y = 0; y < spritesheet.height; y += 32) {
+            let img = spritesheet.get(x, y, 32, 32);
+            image(img, x, y);
+            textures.push(img);
+        }
+    }
+
+    for (let i = 0; i < 400; i++) {
+        let x = random(width);
+        let y = random(height);
+        let design = random(textures);
+        snow.push(new Snowflake(x, y, design));
+    }
+
+    wood = [];
+    wood.push(new Plant(0.42, 0));
+    wood.push(new Plant(0.56,0));
+    // for(let w = 0; w < 2; w++) {
+    //     wood.push(new Plant(w/10, 0));
+    // }
+    c = 0;
+    frameRate(30);
+    textFont(font,getFontSize());
+}
+
+function draw() {
+    background("#999999");
+    for (tree of wood) {
+        push();
+        translate(tree.xpos, tree.ypos);
+        rotate(-0.5*PI);
+        tree.display();
+        pop();    
+    }
+    for (tree of wood) {
+        if (c < 10) {
+            tree.expand();
+        }
+    }
+    if (c < 10) {
+        c++;
+    }
+    
+    zOff += 0.1;
+
+    for (flake of snow) {
+        let xOff = flake.pos.x / width;
+        let yOff = flake.pos.y / height;
+        let wAngle = noise(xOff, yOff, zOff) * TWO_PI;
+        let wind = p5.Vector.fromAngle(wAngle);
+        wind.mult(0.1);
+
+        flake.applyForce(gravity);
+        //flake.applyForce(wind);
+        flake.update();
+        flake.render();
+    }
+
+    fill("#ffffff");
+    textFont(font,getFontSize());
+    text("Merry", width*0.1, 150);
+    text("Christmas", width*0.1, 250);
+    textFont(font,getFontSize()-35);
+    text("and a Happy New Year", width*0.1, 350)
+    textFont(font,getFontSize()-45);
+    text("love from px cx sx xx", width*0.1, height-150)
+
+
+    // for (let point of happy) {	
+	// 	const posX = point.x;
+	// 	const posY = point.y;
+		
+	// 	circle(posX, posY, 2);
+	// 	// if (random(100) > 90) {
+	// 	// 	bigBubblePositions.push({x: posX, y: posY});
+	// 	// }
+	// }
+}
+
+function getFontSize() {
+    let f = height/10;
+    if (f > 100) {
+        f = 100;
+    }
+    return f;
+}
+
+// function getFontOutlinePointsForWord(word) {
+// 	return myFont.textToPoints(word, 55, height / 2, 220, {
+// 		sampleFactor: 0.3,
+// 		simplifyThreshold: 0
+// 	});
+// }
